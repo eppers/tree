f0476688193d722ff15c25b28c5fb061af4b7922
piotrm <piotrm@pc18.Megat.local>
2013-02-28 14:51:29 +0100

    Pricelist services

--- template/cennik-uslugi.php
+++ template/cennik-uslugi.php
@@ -5,67 +5,88 @@
                      {% include 'left-boxes.php' %}
                     <div id="main-container" class="cennik-uslugi">
                         <h2>
-                            Cennik <span>(cena za m2)</span>
+                            Cennik <span>USŁUG</span>
                         </h2>
+                        <p>Zapytaj nas o usługę, która Państwa interesuje, a nie znajduje się na poniższej liście.</p>
                         <div id="cennik-uslugi-container">
                             <div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
                             <div class="viewport">
                                 <div class="overview">
                                     <div class="service-container">
                                         <h4>Pielegnacja ogrodu</h4>
+                                        <p>Usługi wyceniane są indywidualnie.</p>
                                         <ul class="service-list">
                                             <li>
-                                                <p>Pielęgnacja ogrodu</p>
-                                                <p class="service-price">od 100 zl</p>
+                                                <p>Wiercenie otworów pod sadzonki – do 25cm</p>
+                                                <p class="service-price">2,00 – 5,00 zł/szt</p>
                                             </li>
                                             <li>
-                                                <p>Strzyżenie</p>
-                                                <p class="service-price">od 25 zl</p>
+                                                <p>Wiercenie otworów pod słupki do 120 cm</p>
+                                                <p class="service-price">5,00 – 12,00 zł/szt</p>
                                             </li>
                                             <li>
-                                                <p>Wycinka</p>
-                                                <p class="service-price">od 25 zl</p>
+                                                <p>Karczowanie, usuwanie samosiewów</p>
+                                                <p class="service-price">3,00-15,00 zł/szt</p>
                                             </li>
                                             <li>
-                                                <p>Podlewanie trawników</p>
-                                                <p class="service-price">od 25 zl</p>
+                                                <p>Sadzenie (robocizna + podłoże + nawozy)</p>
+                                                <p class="service-price">4,00-20,00 zł/szt</p>
                                             </li>
                                             <li>
-                                                <p>Wypędzanie kretów</p>
-                                                <p class="service-price">od 225 zl</p>
+                                                <p>Koszenie</p>
+                                                <p class="service-price">0,20-0,70 zł/m2</p>
                                             </li>
                                             <li>
-                                                <p>Odbieranie prania</p>
-                                                <p class="service-price">od 15 zl</p>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                     <div class="service-container">
-                                        <h4>Pielegnacja ogrodu</h4>
-                                        <ul class="service-list">
-                                            <li>
-                                                <p>Pielęgnacja ogrodu</p>
-                                                <p class="service-price">od 100 zl</p>
+                                                <p>Nawożenie</p>
+                                                <p class="service-price">0,35-0,50 zł/m2</p>
                                             </li>
                                             <li>
-                                                <p>Strzyżenie</p>
-                                                <p class="service-price">od 25 zl</p>
+                                                <p>Wertykulacja , piaskowanie</p>
+                                                <p class="service-price">0,20-1,50 zł/m2</p>
                                             </li>
                                             <li>
-                                                <p>Wycinka</p>
-                                                <p class="service-price">od 25 zl</p>
+                                                <p>Formowanie krzewów</p>
+                                                <p class="service-price">1,00-30,00 zł/szt</p>
                                             </li>
                                             <li>
-                                                <p>Podlewanie trawników</p>
-                                                <p class="service-price">od 25 zl</p>
+                                                <p>Prześwietlanie owocówek</p>
+                                                <p class="service-price">od 15zł/szt</p>
                                             </li>
                                             <li>
-                                                <p>Wypędzanie kretów</p>
-                                                <p class="service-price">od 225 zl</p>
+                                                <p>Cięcie żywopłotu</p>
+                                                <p class="service-price">2,00-5,00 zł/m2</p>
                                             </li>
                                             <li>
-                                                <p>Odbieranie prania</p>
-                                                <p class="service-price">od 15 zl</p>
+                                                <p>Równanie terenu</p>
+                                                <p class="service-price">6,00-10,00 zł/m2</p>
+                                            </li>
+                                            <li>
+                                                <p>Trawnik z siewu</p>
+                                                <p class="service-price">5,00-15,00 zł/m2</p>
+                                            </li>
+                                            <li>
+                                                <p>Plecakowym oprysk</p>
+                                                <p class="service-price">1szy 70zl zbiornik, kolejny 50zł</p>
+                                            </li>
+                                            <li>
+                                                <p>Pielenie</p>
+                                                <p class="service-price">0,30-1,20 zł/m2</p>
+                                            </li>
+                                            <li>
+                                                <p>Korowanie</p>
+                                                <p class="service-price">10,00 zł/m2</p>
+                                            </li>
+                                            <li>
+                                                <p>Mycie mchu i glonów z kostki</p>
+                                                <p class="service-price">6,00-20,00 zł/m2</p>
+                                            </li>
+                                            <li>
+                                                <p>Grabienie</p>
+                                                <p class="service-price">0,60-2,00 zł/m2</p>
+                                            </li>
+                                            <li>
+                                                <p>Odśnieżanie</p>
+                                                <p class="service-price">1,50-5,00 zł/m2</p>
                                             </li>
                                         </ul>
                                     </div>
